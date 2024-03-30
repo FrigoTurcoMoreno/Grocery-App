1ed0d785602c2c0ea00b73748af3d5a68078cca6
FrigoTurcoMoreno <frigoturcomoreno@gmail.com>
2024-03-30 20:12:35 +0100

    Modified the theme and the colors in the home view. Start using the scaffold in the home view

--- app/src/main/java/com/mftjc/spesa/ui/theme/Color.kt
+++ app/src/main/java/com/mftjc/spesa/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.mftjc.spesa.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green = Color(0xFFA5DD9B)
+val LightWhite = Color(0xFFFEFDED)
+val LightGreen = Color(0xFFC5EBAA)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green2 = Color(0xFF79AC78)
+val LightWhite2 = LightWhite
+val LightGreen2 = Color(0xFFB0D9B1)
--- app/src/main/java/com/mftjc/spesa/view/HomeView.kt
+++ app/src/main/java/com/mftjc/spesa/view/HomeView.kt
@@ -1,58 +1,94 @@
 package com.mftjc.spesa.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.mftjc.spesa.model.Product
 import com.mftjc.spesa.navigation.Screen
+import com.mftjc.spesa.ui.theme.Green
+import com.mftjc.spesa.ui.theme.LightGreen
+import com.mftjc.spesa.ui.theme.LightWhite
 import com.mftjc.spesa.viewmodel.ProductVm
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(
     vm: ProductVm,
     navHostController: NavHostController
 ){
-    Column(modifier = Modifier
-        .fillMaxSize(),
-        Arrangement.Center,
-        Alignment.CenterHorizontally
-    ){
-        Box(modifier = Modifier
-            .fillMaxHeight(0.8f)
-        ) {
-            ShowListProducts(navHostController, vm)
+
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                Text(text = "Spesa")
+                },
+                actions = {
+                    IconButton(onClick = {
+                        vm.deleteAllProducts()
+                    }){
+                        Icon(Icons.Filled.Delete, contentDescription = "Delete all products")
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(Green),
+            )
         }
-        IconButton(
-            onClick = {
-                navHostController.navigate(Screen.AddProductScreen.route)
+    ) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)
+            .background(LightWhite),
+            contentAlignment = Alignment.TopCenter
+        ){
+            ShowListProducts(navHostController = navHostController, vm = vm)
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp),
+                contentAlignment = Alignment.BottomEnd
+            ){
+                FloatingActionButton(
+                    onClick = { navHostController.navigate(Screen.AddProductScreen.route)},
+                    containerColor = Green
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = "Show AddProductScreen")
+                }
             }
-        ) {
-            Icon(Icons.Filled.Add, contentDescription = "Show AddProductScreen")
+
         }
     }
 }
@@ -84,8 +120,10 @@ private fun ProductCard(
     Card(
         modifier = Modifier
             .fillMaxWidth(0.9f)
-            .height(75.dp)
             .padding(10.dp)
+            .shadow(5.dp, shape = RoundedCornerShape(5.dp)),
+        shape = RoundedCornerShape(5.dp),
+        colors = CardDefaults.cardColors(LightGreen)
     ) {
         Row(
             modifier = Modifier
@@ -94,6 +132,9 @@ private fun ProductCard(
             horizontalArrangement = Arrangement.SpaceAround
         ) {
             Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(0.5f),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
@@ -103,6 +144,9 @@ private fun ProductCard(
                 )
             }
             Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(0.5f),
                 contentAlignment = Alignment.Center
             ){
                 Row(
